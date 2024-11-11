69f8148af30270497ab10d1eb021494be16a83de
Nineck <nhournoux@uanl.edu.mx>
2024-11-11 16:46:50 -0600

    Cambio a base de datos mysql

--- gestor_recetas/settings.py
+++ gestor_recetas/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'gestor_recetas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            "read_default_file": "~/my.cnf"
+        },
     }
 }
 
